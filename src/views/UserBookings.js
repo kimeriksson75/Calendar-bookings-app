674357f5dd28b7b18767599a8b5be4a590031b85
kimeriksson75 <kimeriksson75@gmail.com>
2020-05-24 18:20:48 +0200

    filter old bookings and updated grid

--- src/views/UserBookings.js
+++ src/views/UserBookings.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import history from '../history';
 import { Sidebar, Segment, Icon } from 'semantic-ui-react';
 import moment from 'moment';
-import _, { sortBy, omit, map } from 'lodash/fp';
-import { getBookingByAuthor, newMessage, toggleSidebar } from '../actions';
+import { pipe, sortBy, filter } from 'lodash/fp';
+import { getBookingByAuthor, newMessage } from '../actions';
 import PusherHeader from '../components/PusherHeader';
 
 const UserBookings = props => {
@@ -11,20 +12,22 @@ const UserBookings = props => {
     auth: { user, isSignedIn },
     bookingData: { userBookings },
     getBookingByAuthor,
-    newMessage,
-    toggleSidebar
+    newMessage
   } = props;
 
 
-  const sortedUserBookings = _.pipe(
+  const sortedUserBookings = pipe([
     sortBy(booking => new Date(booking.date)),
-    map(booking => omit([new Date(booking.date) < new Date()], booking))
-  )(userBookings);
+    filter(booking => new Date(booking.date) > new Date())
+  ])(userBookings);
 
   useEffect(() => {
     if (user) getBookingByAuthor(user._id);
   }, [user, getBookingByAuthor]);
 
+  const onBookingClick = url => {
+    history.push(url);
+  }
 
   const renderDate = date => {
     const updatedDate = moment(date);
@@ -33,22 +36,31 @@ const UserBookings = props => {
 
   const renderTimeslots = timeslots =>
     timeslots.map((timeslot) => timeslot.userId === user._id ?
-      (<li key={timeslot.id} className="item">{`${timeslot.timeslot}`}</li>) : null)
+      (<div className="ui item">
+        <div key={timeslot.id} className="ui label">
+          <i className="check icon teal"></i>
+          {timeslot.timeslot}
+        </div>
+      </div>) : null)
 
   const renderBookings = () =>
     sortedUserBookings.map((booking) => {
       const timeslots = renderTimeslots(booking.timeslots);
       return Boolean(timeslots.find(value => value !== null)) ? (
-        <div key={booking.id} className="ui relaxed divided list">
-          <a className="item" href={`/calendar/${moment(booking.date).format('YYYY')}/${moment(booking.date).format('MM')}/${moment(booking.date).format('DD')}`}>
-            <i className="large calendar check green outline icon"></i>
-            <div className="content">
-              <div className="header" style={{ textTransform: 'capitalize' }} >{renderDate(booking.date)}</div>
-              <ul className="description">
-                {timeslots}
-              </ul>
+        <div className="ui celled grid">
+          <div key={booking.id} className="three column row" onClick={() => onBookingClick(`/calendar/${moment(booking.date).format('YYYY')}/${moment(booking.date).format('MM')}/${moment(booking.date).format('DD')}`)}>
+            <div className="two wide column">
+              <i className="large calendar check green centered outline icon"></i>
             </div>
-          </a>
+            <div className="five wide column">
+              <div className="ui label" style={{ textTransform: 'capitalize' }} >{renderDate(booking.date)}</div>
+            </div>
+            <div className="four wide column">
+              <div className="ui divided items">
+                {timeslots}
+              </div>
+            </div>
+          </div>
         </div>
       ) : null;
     });
@@ -66,7 +78,7 @@ const UserBookings = props => {
       <Segment basic>
         <PusherHeader title="Mina bokningar" subTitle="Kommande sorterat i datumordning" />
         {isSignedIn ?
-          (<div className="ui container">
+          (<div>
             {userBookings && renderBookings()}
           </div>) : userErrorMessage()
         }
@@ -81,4 +93,4 @@ const mapStateToProps = (state) => {
     bookingData: state.bookingData
   });
 }
-export default connect(mapStateToProps, { getBookingByAuthor, newMessage, toggleSidebar })(UserBookings);
+export default connect(mapStateToProps, { getBookingByAuthor, newMessage })(UserBookings);
