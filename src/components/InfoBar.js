f339518ea7ba44b257e2c4f561f8b0289a05fa75
kimeriksson75 <kimeriksson75@gmail.com>
2023-11-28 15:08:16 +0100

    infobar description

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const InfoBar = ({ title }) => {
+const InfoBar = ({ title, description }) => {
     return (
         <div className="info-bar">
             <div className="info-bar__title">
                 <h3>{title}</h3>
+                {description && <p>{description}</p>}
             </div>
         </div>
     );
